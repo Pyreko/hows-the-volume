25b4339b62ebf7797e6e1d69b8d3bffa84543bc7
Pyreko <25498386+Pyreko@users.noreply.github.com>
2022-07-02 16:14:05 -0400

    send sigint in restart

--- server/restart.sh
+++ server/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkill hows-the-volume
+pkill -2 hows-the-volume
 mkdir -p ~/htv-server
 cp ./target/release/hows-the-volume-server ~/htv-server/hows-the-volume-server
 nohup ~/htv-server/hows-the-volume-server &
