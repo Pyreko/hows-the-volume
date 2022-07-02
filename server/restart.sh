5223112bb87c47852d5b6751428902caf04a55a1
Pyreko <25498386+Pyreko@users.noreply.github.com>
2022-07-02 16:20:13 -0400

    make sure to cd to the right folder

--- server/restart.sh
+++ server/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pkill -2 hows-the-volume
-mkdir -p ~/htv-server
 cp ./target/release/hows-the-volume-server ~/htv-server/hows-the-volume-server
-nohup ~/htv-server/hows-the-volume-server &
+cd ~/htv-server
+nohup ./hows-the-volume-server &
