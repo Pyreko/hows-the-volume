e156294e892d49792a172d295a29a98f22017805
Pyreko <25498386+Pyreko@users.noreply.github.com>
2022-07-02 20:40:33 -0400

    add asset copying to restart script

--- server/restart.sh
+++ server/restart.sh
@@ -5,5 +5,6 @@ set -eu
 pkill -2 hows-the-volume
 while pgrep -u $UID -x hows-the-volume >/dev/null; do sleep 1; done
 cp ./target/release/hows-the-volume-server ~/htv-server/hows-the-volume-server
+cp -r ./assets ~/htv-server/
 cd ~/htv-server
 nohup ./hows-the-volume-server > /dev/null 2>&1 &
