583d9f1c6da1fcd5857f79958c721f770d882108
Pyreko <25498386+Pyreko@users.noreply.github.com>
2022-07-11 02:55:56 -0400

    disable diamonds for now

---
+++ site/src/lib/components/Diamonds.svelte
@@ -0,0 +1,13 @@
+<div class="diamonds" />
+
+<style>
+	.diamonds {
+		position: absolute;
+		z-index: -50;
+		width: 100vw;
+		height: 100vh;
+		background-color: rgba(0, 0, 0, 0.01);
+		-webkit-mask-image: url('/images/diamond.svg');
+		mask-image: url('/images/diamond.svg');
+	}
+</style>
