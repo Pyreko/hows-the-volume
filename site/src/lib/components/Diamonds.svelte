d25b15289475f0e30394558fd0bc8391434e9843
Pyreko <25498386+Pyreko@users.noreply.github.com>
2022-08-18 01:00:43 -0400

    further tweak diamond sizes and reactivity

--- site/src/lib/components/Diamonds.svelte
+++ site/src/lib/components/Diamonds.svelte
@@ -17,9 +17,30 @@
 		background-color: rgba(0, 0, 0, 0.025);
 		-webkit-mask-image: url('/images/diamond.svg');
 		mask-image: url('/images/diamond.svg');
-		-webkit-mask-size: 1200px 800px;
-		mask-size: 1200px 800px;
+		-webkit-mask-size: 600px 400px;
+		mask-size: 600px 400px;
 		-webkit-mask-position: center;
 		mask-position: center;
+	}
+
+	@media only screen and (min-width: 480px) {
+		.diamonds {
+			-webkit-mask-size: 900px 600px;
+			mask-size: 900px 600px;
+		}
+	}
+
+	@media only screen and (min-width: 768px) {
+		.diamonds {
+			-webkit-mask-size: 1200px 800px;
+			mask-size: 1200px 800px;
+		}
+	}
+
+	@media only screen and (min-width: 1024px) {
+		.diamonds {
+			-webkit-mask-size: 1500px 1000px;
+			mask-size: 1500px 1000px;
+		}
 	}
 </style>
