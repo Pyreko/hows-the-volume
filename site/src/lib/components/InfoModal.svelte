61af31c6020cf548b5fdcaa23c4a19cc36d7127c
Pyreko <25498386+Pyreko@users.noreply.github.com>
2022-08-18 01:11:57 -0400

    tweak modal background colour

--- site/src/lib/components/InfoModal.svelte
+++ site/src/lib/components/InfoModal.svelte
@@ -133,7 +133,7 @@
 		top: 0;
 		width: 100%;
 		height: 100%;
-		background-color: rgba(0, 0, 0, 0.5);
+		background-color: rgba(75, 75, 75, 0.5);
 		justify-content: center;
 		align-items: center;
 	}
