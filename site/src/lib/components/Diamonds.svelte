f7d6cf63852ab079499f5e9276d7d6535135c370
Pyreko <25498386+Pyreko@users.noreply.github.com>
2022-08-18 00:37:51 -0400

    update how diamonds are done

--- site/src/lib/components/Diamonds.svelte
+++ site/src/lib/components/Diamonds.svelte
@@ -17,5 +17,9 @@
 		background-color: rgba(0, 0, 0, 0.025);
 		-webkit-mask-image: url('/images/diamond.svg');
 		mask-image: url('/images/diamond.svg');
+		-webkit-mask-size: 1200px 800px;
+		mask-size: 1200px 800px;
+		-webkit-mask-position: center;
+		mask-position: center;
 	}
 </style>
