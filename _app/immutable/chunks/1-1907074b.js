48926e091477fda6463ddea206586125c5a19e99
Pyreko <Pyreko@users.noreply.github.com>
2022-11-27 03:34:43 +0000

    deploy: 6c052d2671a0bec84362cdec822e8721ebc15cb6

--- _app/immutable/chunks/1-1907074b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-a9832cc1.js";export{t as component};
---
+++ _app/immutable/chunks/2-ba0cd1bd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-76a248fb.js";export{t as component};
