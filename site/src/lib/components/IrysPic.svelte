e50468b17036d2daeeeb85cdd173374fc043c7bb
Pyreko <25498386+Pyreko@users.noreply.github.com>
2022-11-27 01:42:20 -0500

    less jank way of doing this

--- site/src/lib/components/IrysPic.svelte
+++ site/src/lib/components/IrysPic.svelte
@@ -1,23 +1,21 @@
 <script lang="ts">
 	import { clickOpacity } from '$lib/store';
 
-	function loadImage() {
-		let rand = Math.floor(Math.random() * (10 + 1));
-
-		if (rand >= 5) {
-			return {
+	let loadedImage = (() => {
+		const choices = [
+			{
 				img: '/images/wink.webp',
 				alt: 'A winking IRyS.'
-			};
-		} else {
-			return {
+			},
+			{
 				img: '/images/smug.webp',
 				alt: 'A smug IRyS.'
-			};
-		}
-	}
+			}
+		];
 
-	let loadedImage = loadImage();
+		let rand = Math.floor(Math.random() * choices.length);
+		return choices[rand];
+	})();
 	let imageSrc = loadedImage.img;
 	let imageAlt = loadedImage.alt;
 </script>
