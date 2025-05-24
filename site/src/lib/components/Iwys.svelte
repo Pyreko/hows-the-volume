55c00f9c2b59b86f4d25a7f6e8b985a637862af5
Pyreko <25498386+Pyreko@users.noreply.github.com>
2025-05-24 01:26:29 -0400

    Some fun around names (#33)
    * some fun
    
    * fmt

--- site/src/lib/components/Iwys.svelte
+++ site/src/lib/components/Iwys.svelte
@@ -1,14 +1,44 @@
 <script lang="ts">
+	import { randomInt } from '$lib/store';
+
 	const SPOTIFY_LINK = 'https://open.spotify.com/artist/7y1hO3QeftnOJmWCbzeZav';
 	const TWITTER_LINK = 'https://twitter.com/irys_en';
 	const YOUTUBE_LINK = 'https://www.youtube.com/@IRyS';
+	const APPLE_MUSIC_LINK = 'https://music.apple.com/jp/artist/irys/1576219850';
+
+	const nephilimLabels = [
+		'Hope',
+		'the nephilim',
+		'███████ IRyS',
+		'the "seiso" nephilim',
+		'the seiso nephilim',
+		'the pon',
+		'the Embodiment of HOPE',
+		'the keyboard enthusiast',
+		'Umino Milk',
+		'The Hot Pink One',
+		['the Diamond Girlfriend', 'https://www.youtube.com/watch?v=EXM0EE_NpQc']
+	];
+
+	const IRYS_LABEL_OFFSET = 20;
+	const nephilimLabelIndex = randomInt(nephilimLabels.length - 1 + IRYS_LABEL_OFFSET);
+	export const nephilimLabel =
+		nephilimLabelIndex <= IRYS_LABEL_OFFSET
+			? 'IRyS'
+			: nephilimLabels[nephilimLabelIndex - IRYS_LABEL_OFFSET];
 </script>
 
 <div>
 	<p>
-		Support IRyS on <a href={YOUTUBE_LINK} target="_blank" rel="noopener noreferrer">YouTube</a>,
-		<a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer">Twitter</a>, and
-		<a href={SPOTIFY_LINK} target="_blank" rel="noopener noreferrer">Spotify</a>!
+		Support {#if Array.isArray(nephilimLabel)}<a
+				href={nephilimLabel[1]}
+				target="_blank"
+				rel="noopener noreferrer">{nephilimLabel[0]}</a
+			>{:else}{nephilimLabel}{/if} on
+		<a href={YOUTUBE_LINK} target="_blank" rel="noopener noreferrer">YouTube</a>,
+		<a href={TWITTER_LINK} target="_blank" rel="noopener noreferrer">Twitter</a>,
+		<a href={SPOTIFY_LINK} target="_blank" rel="noopener noreferrer">Spotify</a>, and
+		<a href={APPLE_MUSIC_LINK} target="_blank" rel="noopener noreferrer">Apple Music</a>!
 	</p>
 </div>
 
